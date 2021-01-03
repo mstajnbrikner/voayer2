5d1ced5d22b5b8a7b19d7fe568eadd20eeffbe90
Matej Stajnbrikner <stambi28@gmail.com>
2021-01-03 12:15:37 +0100

    initial commit

---
+++ cameraAndVideo/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace cameraAndVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cameraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.startRecordingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopRecordingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1070, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cameraToolStripMenuItem,
+            this.startRecordingToolStripMenuItem,
+            this.stopRecordingToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // cameraToolStripMenuItem
+            // 
+            this.cameraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.pauseToolStripMenuItem});
+            this.cameraToolStripMenuItem.Name = "cameraToolStripMenuItem";
+            this.cameraToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cameraToolStripMenuItem.Text = "Camera";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.pauseToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(496, 510);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(514, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(544, 510);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // startRecordingToolStripMenuItem
+            // 
+            this.startRecordingToolStripMenuItem.Name = "startRecordingToolStripMenuItem";
+            this.startRecordingToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.startRecordingToolStripMenuItem.Text = "Start recording";
+            this.startRecordingToolStripMenuItem.Click += new System.EventHandler(this.startRecordingToolStripMenuItem_Click);
+            // 
+            // stopRecordingToolStripMenuItem
+            // 
+            this.stopRecordingToolStripMenuItem.Name = "stopRecordingToolStripMenuItem";
+            this.stopRecordingToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.stopRecordingToolStripMenuItem.Text = "Stop recording";
+            this.stopRecordingToolStripMenuItem.Click += new System.EventHandler(this.stopRecordingToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 549);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cameraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem startRecordingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopRecordingToolStripMenuItem;
+    }
+}
+
---
+++ cameraAndVideo/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace cameraAndVideo
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+        VideoWriter writer;
+
+        DateTime dateTime;
+        DateTime dateTime_finish;
+        int frameCount = 0;
+        System.Threading.Timer timer;
+
+        bool setupFinished = false;
+
+        bool recordingEnabled = false;
+        private int framesRecorded;
+        private int maxFrames;
+        string destinationPath = @"D:\Google_drive\Visual Studio - programi\EMGU CV\";
+        int videoNumber = 0;
+        double fps = 0;
+
+
+
+        Mat m = new Mat();
+        Mat last_m = new Mat();
+
+
+
+        Image<Gray, byte> currentFrame;
+        Image<Gray, byte> lastFrame;
+        Image<Gray, byte> diffFrame;
+
+        bool firstFrame = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Console.WriteLine(destinationPath);
+
+        }
+
+
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                capture = new Emgu.CV.VideoCapture(0);
+            }
+            //capture.ImageGrabbed += Capture_ImageGrabbed;              
+            capture.Start();
+
+            dateTime = DateTime.Now;
+            dateTime_finish = dateTime.AddMilliseconds(10000);
+            frameCount = 0;
+
+            timer = new System.Threading.Timer(new TimerCallback(TickTimer), null, 1000, 200);
+
+        }
+
+        private void TickTimer(object state)
+        {
+            int moves;
+            frameCount++;
+
+            if (DateTime.Now > dateTime_finish && setupFinished == false)
+            {
+                setupFinished = true;
+                //fps = (double)frameCount / (double)10;
+            }
+
+
+            try
+            {
+                if (firstFrame)
+                {
+                    capture.Read(m);
+                    pictureBox1.Image = m.ToBitmap();
+                    firstFrame = false;
+                    last_m = m.Clone();
+                }
+                else
+                {
+                    capture.Read(m);
+
+
+                    currentFrame = m.ToImage<Gray, byte>();
+                    lastFrame = last_m.ToImage<Gray, byte>();
+
+                    pictureBox1.Image = currentFrame.ToBitmap();
+
+                    diffFrame = currentFrame.AbsDiff(lastFrame);
+                    moves = 0;
+                    for (int i = 0; i < diffFrame.Rows; i += 10)
+                    {
+                        for (int j = 0; j < diffFrame.Cols; j += 10)
+                        {
+                            if (diffFrame.Data[i, j, 0] > 25)
+                            {
+                                moves++;
+                            }
+                        }
+                    }
+                    Console.WriteLine(moves.ToString());
+
+                    // moves = diffFrame.CountNonzero()[0];
+                    Console.WriteLine(moves);
+
+                    if ((moves > 20) && (recordingEnabled == false) && (DateTime.Now > dateTime_finish))
+                    {
+                        recordingEnabled = true;
+                        maxFrames = 100;
+                        framesRecorded = 0;
+                    }
+                    if ((moves > 20) && (recordingEnabled == true) && (framesRecorded > (maxFrames - 40)))
+                    {
+                        maxFrames += 40;
+                        Console.WriteLine("Dodano 40 okvira!!");
+                    }
+
+
+                    //pictureBox2.Image = diffFrame.ToBitmap();
+
+                    last_m = m.Clone();
+                }
+
+                if ((recordingEnabled == true) && (DateTime.Now > dateTime_finish))
+                {
+
+                    if (writer == null)
+                    {
+                        int fourcc = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FourCC));
+                        int width = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth));
+                        int height = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight));
+                        writer = new VideoWriter(destinationPath + "recorded" + (videoNumber + 1).ToString() + ".mp4", fourcc, 5, new Size(width, height), false);
+                        videoNumber++;
+                    }
+                    writer.Write(m);
+                    framesRecorded++;
+                    if (framesRecorded >= maxFrames)
+                    {
+                        framesRecorded = 0;
+                        recordingEnabled = false;
+                        writer.Dispose();
+                        writer = null;
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        
+
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            recordingEnabled = false;
+            timer.Change(Timeout.Infinite, Timeout.Infinite);
+            timer.Dispose();
+            timer = null;
+            if (capture != null)
+            {
+                capture.Dispose();
+                capture = null;
+            }
+            if (writer.IsOpened)
+            {
+                writer.Dispose();
+                writer = null;
+            }
+        }
+
+        private void pauseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (capture != null)
+            {
+                capture.Pause();
+            }
+        }
+
+        private void startRecordingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                return;
+            }
+
+            if (writer == null)
+            {
+                int fourcc = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FourCC));
+                int width = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth));
+                int height = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight));
+                writer = new VideoWriter(destinationPath, fourcc, fps, new Size(width, height), false);
+            }
+            recordingEnabled = true;
+        }
+
+        private void stopRecordingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            writer.Dispose();
+            writer = null;
+            recordingEnabled = false;
+        }
+    }
+}
